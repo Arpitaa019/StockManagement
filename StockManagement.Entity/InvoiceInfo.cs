71e0d28982031394e1bbac4c822a031371cfcd23
Arpitaa019 <110777404+Arpitaa019@users.noreply.github.com>
2026-04-06 17:38:06 +0530

    Maering to Master (#1)
    * dfgsdf
    
    * asdfa
    
    * asdf
    
    * asdfasd

--- StockManagement.Entity/InvoiceInfo.cs
+++ StockManagement.Entity/InvoiceInfo.cs
@@ -11,10 +11,13 @@ namespace StockManagement.Entity
     /// <summary>
     /// - Created after goods are received.
     /// // Contains product details, vendor info, and payment terms.
+    /// asdfasd
+    /// asdfasd
     /// </summary>
     internal class InvoiceInfo
     {
-
+        //adsfasd
+        //ddd
         //hsllo
         //byb yhe
     }
